e3a7095fd273a44e05446293f0dae464f5315f12
acupoftee <bdiakaba@uccs.edu>
2019-03-01 20:54:25 -0700

    fixed color bug

--- src/commands/LiveCommand.js
+++ src/commands/LiveCommand.js
@@ -36,28 +36,29 @@ class LiveCommand extends Command {
         let match = new Match(live.id, (live.state === 'PENDING') ? true : false, live.state,
             live.startDateTS, home, away, scoreHome, scoreAway);
 
-        let banner = new Banner(home.primaryColor, away.primaryColor, home.logo, away.logo);
+        let banner = new Banner(`#${live.competitors[0].primaryColor}`, `#${live.competitors[1].primaryColor}`, home.logo, away.logo);
         let pacificTime = moment_timezone(match.startDateTS).tz('America/Los_Angeles').format('h:mm A z');
         let utcTime = moment_timezone(match.startDateTS).utc().format('h:mm A z');
 
         if (match.state === 'IN_PROGRESS') {
             embed.setTitle(`__NOW LIVE: ${moment_timezone(match.startDateTS).tz('America/Los_Angeles').format('ddd. MMM Do, YYYY')}__`);
-            embed.setDescription(`${Emojis[match.home.abbreviatedName]}**${match.home.name}** ||${match.scoreHome}-${
-                match.scoreAway}|| **${match.away.name}** ${Emojis[match.away.abbreviatedName]}`);
-            embed.setThumbnail(null);
+            embed.setDescription(`*${pacificTime} / ${utcTime}*\n**${match.home.name}** ||${match.scoreHome}-${
+                match.scoreAway}|| **${match.away.name}**`);
+            embed.setThumbnail("https://cdn.discordapp.com/emojis/551245013938470922.png?v=1");
             banner.buildBanner();
+            embed.setColor(banner.homePrimaryColor);
+            embed.setImageFileName('src/res/banner.png', 'banner.png');
         } else if (match.pending) {
             embed.setTitle(`__Next Live Match: ${moment_timezone(match.startDateTS).tz('America/Los_Angeles').format('ddd. MMM Do, YYYY')}__`);
             embed.setDescription(`*${pacificTime} / ${utcTime}*\n${
                 Emojis[match.home.abbreviatedName]} **${match.home.name}** vs **${
                 match.away.name}** ${Emojis[match.away.abbreviatedName]}\nStarts ${
                 moment_timezone(match.startDateTS).endOf('hour').fromNow()}\n`);
-            banner.buildBanner();
+            //banner.buildBanner();
         } else {
             MessageUtil.sendSuccess(message.channel, "Check back later for the next match!");
             return;
         }
-        embed.setImageFileName('src/res/banner.png', 'banner.png');
         embed.buildEmbed().post(message.channel);
     }
 }
--- src/models/owl_models/Banner.js
+++ src/models/owl_models/Banner.js
@@ -25,39 +25,42 @@ class Banner {
             this._awayLogo = awayLogo;
         }
 
-        /**
-         * Constructs a banner displaying the teams that are 
-         * currently facing off in an Overwatch League Match
-         * @returns {Attachment} a new Image attachment
-         */
-        async buildBanner() {
-            const canvas = createCanvas(500, 250);
-            const ctx = canvas.getContext('2d');
-            let width = 150, height = 150;
+    get homePrimaryColor() {
+        return this._homePrimaryColor;
+    }
+    /**
+     * Constructs a banner displaying the teams that are 
+     * currently facing off in an Overwatch League Match
+     * @returns {Attachment} a new Image attachment
+     */
+    async buildBanner() {
+        const canvas = createCanvas(500, 250);
+        const ctx = canvas.getContext('2d');
+        let width = 150, height = 150;
 
-            ctx.fillStyle = this._homePrimaryColor;
-            ctx.fillRect(0, 0, 250, 250);
+        ctx.fillStyle = this._homePrimaryColor;
+        ctx.fillRect(0, 0, 250, 250);
 
-            ctx.fillStyle = this._awayPrimaryColor;
-            ctx.fillRect(250, 0, 250, 250);
+        ctx.fillStyle = this._awayPrimaryColor;
+        ctx.fillRect(250, 0, 250, 250);
 
-            let homeLogo = await loadImage(this._homeLogo);
-            ctx.drawImage(homeLogo, 
-                    ((canvas.width / 2) - width) / 2,
-                    canvas.height / 2 - height / 2, width, height);
-            homeLogo.onerror = err => { throw err };
+        let homeLogo = await loadImage(this._homeLogo);
+        ctx.drawImage(homeLogo, 
+                ((canvas.width / 2) - width) / 2,
+                canvas.height / 2 - height / 2, width, height);
+        homeLogo.onerror = err => { throw err };
 
-            let awayLogo = await loadImage(this._awayLogo);
-             ctx.drawImage(awayLogo, 
-                ((canvas.width / 2) - width) * 3,
-                    canvas.height / 2 - height / 2, width, height);
-            awayLogo.onerror = err => { throw err };
+        let awayLogo = await loadImage(this._awayLogo);
+            ctx.drawImage(awayLogo, 
+            ((canvas.width / 2) - width) * 3,
+                canvas.height / 2 - height / 2, width, height);
+        awayLogo.onerror = err => { throw err };
 
-            const out = fs.createWriteStream('./src/res/banner.png');
-            const stream = canvas.createPNGStream();
-            stream.pipe(out);
-            out.on('finish', () => console.log('The File was created'));
+        const out = fs.createWriteStream('./src/res/banner.png');
+        const stream = canvas.createPNGStream();
+        stream.pipe(out);
+        out.on('finish', () => console.log('The File was created'));
 
-        }
+    }
 }
 module.exports = Banner;
