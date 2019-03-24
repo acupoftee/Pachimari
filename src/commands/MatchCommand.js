1eac04241952130a2bbfd6f50c97f67c3b12069f
acupoftee <bdiakaba@uccs.edu>
2019-03-24 08:48:24 -0600

    added single name option for matches

--- src/commands/MatchCommand.js
+++ src/commands/MatchCommand.js
@@ -18,66 +18,129 @@ class MatchCommand extends Command {
         let stage_week = "";
         //let loading = message.channel.send(Emojis["LOADING"]);
         message.channel.startTyping();
-        let pages = [], matches = [];
-        let page = 1;
+        let pages = [], matches = [], titles = [];
+        let page = 1, title = 1;
         let header;
         let found = false;
-
-        let firstTeam = CompetitorManager.competitors.get(CompetitorManager.locateTeam(args[0]));
-        let secondTeam = CompetitorManager.competitors.get(CompetitorManager.locateTeam(args[1]));
-
         let embed = new PachimariEmbed(client);
-        embed.setTitle(`${Emojis[firstTeam.abbreviatedName.toUpperCase()]} ${firstTeam.name} vs ${secondTeam.name} ${Emojis[secondTeam.abbreviatedName.toUpperCase()]}`);
-        const body = await JsonUtil.parse(Endpoints.get("SCHEDULE"));
-        for (const _stage of body.data.stages) {
-            for (const week of _stage.weeks) {
-                stage_week = `${_stage.name}/${week.name}`;
-                for (const _match of week.matches) {
-                    let home = CompetitorManager.competitors.get(CompetitorManager.locateTeam(_match.competitors[0].abbreviatedName));
-                    let away = CompetitorManager.competitors.get(CompetitorManager.locateTeam(_match.competitors[1].abbreviatedName));
-                    let homeMatchScore = _match.scores[0].value;
-                    let awayMatchScore = _match.scores[1].value;
-                    if (((home.id === firstTeam.id) && (away.id === secondTeam.id)) || ((away.id === firstTeam.id) && (home.id === secondTeam.id))) {
-                        found = true;
-                        if (_match.status === "CONCLUDED") {
-                            for (let i = 0; i < _match.games.length; i++) {
-                                const mapGuid = _match.games[i].attributes.mapGuid;
-                                const mapName = await MapManager.getMap(mapGuid);
-                                const mapType = await MapManager.getMapType(mapGuid);
-                                let homeMapScore = _match.games[i].points === undefined ? 0 : _match.games[i].points[0];
-                                let awayMapScore = _match.games[i].points === undefined ? 0 : _match.games[i].points[1];
-                                let mapStr = `${Emojis[mapType.toUpperCase()]} ${mapName}: ${mapType}\n${
-                                    Emojis[home.abbreviatedName.toUpperCase()]} ${homeMapScore} - ${awayMapScore} ${
-                                    Emojis[away.abbreviatedName.toUpperCase()]}`
-                                if (i == 0) {
-                                    header = `Match Date: ${moment_timezone(_match.startDateTS).tz('America/Los_Angeles').format('dddd. MMM Do, YYYY')}
+
+        if (args.length < 1) {
+            message.channel.stopTyping();
+            MessageUtil.sendError(message.channel, "Sorry, I couldn't find matches with ony one team name :C Make sure to add two!");
+        } else if (args.length == 1) {
+            let firstTeam = CompetitorManager.competitors.get(CompetitorManager.locateTeam(args[0]));
+            embed.setTitle(`${Emojis[firstTeam.abbreviatedName.toUpperCase()]} ${firstTeam.name} Matches`);
+            embed.setColor(firstTeam.primaryColor);
+            const body = await JsonUtil.parse(Endpoints.get("SCHEDULE"));
+            for (const _stage of body.data.stages) {
+                for (const week of _stage.weeks) {
+                    stage_week = `${_stage.name} - ${week.name}`;
+                    for (const _match of week.matches) {
+                        let home = CompetitorManager.competitors.get(CompetitorManager.locateTeam(_match.competitors[0].abbreviatedName));
+                        let away = CompetitorManager.competitors.get(CompetitorManager.locateTeam(_match.competitors[1].abbreviatedName));
+                        let homeMatchScore = _match.scores[0].value;
+                        let awayMatchScore = _match.scores[1].value;
+                        if ((home.id === firstTeam.id) || ((away.id === firstTeam.id))) {
+                            found = true;
+                            if (_match.status === "CONCLUDED") {
+                                for (let i = 0; i < _match.games.length; i++) {
+                                    const mapGuid = _match.games[i].attributes.mapGuid;
+                                    const mapName = await MapManager.getMap(mapGuid);
+                                    const mapType = await MapManager.getMapType(mapGuid);
+                                    let homeMapScore = _match.games[i].points === undefined ? 0 : _match.games[i].points[0];
+                                    let awayMapScore = _match.games[i].points === undefined ? 0 : _match.games[i].points[1];
+                                    let mapStr = `${Emojis[mapType.toUpperCase()]} ${mapName}: ${mapType}\n${
+                                        Emojis[home.abbreviatedName.toUpperCase()]} ${homeMapScore} - ${awayMapScore} ${
+                                        Emojis[away.abbreviatedName.toUpperCase()]}\n`
+                                    if (i == 0) {
+                                        let title = `${Emojis[home.abbreviatedName.toUpperCase()]} __${home.name} vs. ${away.name}__ ${Emojis[away.abbreviatedName.toUpperCase()]}`;
+                                        titles.push(title);
+                                        header = `Match Date: ${moment_timezone(_match.startDateTS).tz('America/Los_Angeles').format('dddd. MMM Do, YYYY')}
                                         ${stage_week}\n
-                                        Match Score: ${Emojis[home.abbreviatedName.toUpperCase()]} ${homeMatchScore} - ${
-                                        awayMatchScore} ${Emojis[away.abbreviatedName.toUpperCase()]}\n`;
-                                    mapStr = header + mapStr;
+                                        Match Score:\n${Emojis[home.abbreviatedName.toUpperCase()]} ${homeMatchScore} - ${
+                                            awayMatchScore} ${Emojis[away.abbreviatedName.toUpperCase()]}\n\n__**Maps**__\n`;
+                                        mapStr = header + mapStr;
+                                    }
+                                    matches.push(mapStr);
                                 }
-                                matches.push(mapStr);
+                            } else {
+                                let title = `${Emojis[home.abbreviatedName.toUpperCase()]} __${home.name} vs. ${away.name}__ ${Emojis[away.abbreviatedName.toUpperCase()]}`;
+                                titles.push(title);
+                                header = `Match Date: ${moment_timezone(_match.startDateTS).tz('America/Los_Angeles').format('dddd. MMM Do, YYYY')}\n${stage_week}`;
+                                matches.push(header);
                             }
-                        } else {
-                            let header = `Match Date: ${moment_timezone(_match.startDateTS).tz('America/Los_Angeles').format('dddd. MMM Do, YYYY')}\n${stage_week}`;
-                            matches.push(header);
+                        }
+                        if (matches.length > 0) {
+                            pages.push(matches);
+                            matches = [];
                         }
                     }
                 }
             }
-            if (matches.length > 0) {
-                pages.push(matches);
-                matches = [];
+        } else {
+            let firstTeam = CompetitorManager.competitors.get(CompetitorManager.locateTeam(args[0]));
+            let secondTeam = CompetitorManager.competitors.get(CompetitorManager.locateTeam(args[1]));
+
+            embed.setTitle(`${Emojis[firstTeam.abbreviatedName.toUpperCase()]} ${firstTeam.name} vs ${secondTeam.name} ${Emojis[secondTeam.abbreviatedName.toUpperCase()]}`);
+            embed.setColor(firstTeam.primaryColor);
+            const body = await JsonUtil.parse(Endpoints.get("SCHEDULE"));
+            for (const _stage of body.data.stages) {
+                for (const week of _stage.weeks) {
+                    stage_week = `${_stage.name} - ${week.name}`;
+                    for (const _match of week.matches) {
+                        let home = CompetitorManager.competitors.get(CompetitorManager.locateTeam(_match.competitors[0].abbreviatedName));
+                        let away = CompetitorManager.competitors.get(CompetitorManager.locateTeam(_match.competitors[1].abbreviatedName));
+                        let homeMatchScore = _match.scores[0].value;
+                        let awayMatchScore = _match.scores[1].value;
+                        if (((home.id === firstTeam.id) && (away.id === secondTeam.id)) || ((away.id === firstTeam.id) && (home.id === secondTeam.id))) {
+                            found = true;
+                            if (_match.status === "CONCLUDED") {
+                                for (let i = 0; i < _match.games.length; i++) {
+                                    const mapGuid = _match.games[i].attributes.mapGuid;
+                                    const mapName = await MapManager.getMap(mapGuid);
+                                    const mapType = await MapManager.getMapType(mapGuid);
+                                    let homeMapScore = _match.games[i].points === undefined ? 0 : _match.games[i].points[0];
+                                    let awayMapScore = _match.games[i].points === undefined ? 0 : _match.games[i].points[1];
+                                    let mapStr = `${Emojis[mapType.toUpperCase()]} ${mapName}: ${mapType}\n${
+                                        Emojis[home.abbreviatedName.toUpperCase()]} ${homeMapScore} - ${awayMapScore} ${
+                                        Emojis[away.abbreviatedName.toUpperCase()]}\n`
+                                    if (i == 0) {
+                                        let title = `${Emojis[home.abbreviatedName.toUpperCase()]} __${home.name} vs. ${away.name}__ ${Emojis[away.abbreviatedName.toUpperCase()]}`;
+                                        titles.push(title);
+                                        header = `Match Date: ${moment_timezone(_match.startDateTS).tz('America/Los_Angeles').format('dddd. MMM Do, YYYY')}
+                                            ${stage_week}\n
+                                            Match Score:\n${Emojis[home.abbreviatedName.toUpperCase()]} ${homeMatchScore} - ${
+                                            awayMatchScore} ${Emojis[away.abbreviatedName.toUpperCase()]}\n\n__**Maps**__\n`;
+                                        mapStr = header + mapStr;
+                                    }
+                                    matches.push(mapStr);
+                                }
+                            } else {
+                                let title = `${Emojis[home.abbreviatedName.toUpperCase()]} __${home.name} vs. ${away.name}__ ${Emojis[away.abbreviatedName.toUpperCase()]}`;
+                                titles.push(title);
+                                let header = `Match Date: ${moment_timezone(_match.startDateTS).tz('America/Los_Angeles').format('dddd. MMM Do, YYYY')}\n${stage_week}`;
+                                matches.push(header);
+                            }
+                        }
+                    }
+                    if (matches.length > 0) {
+                        pages.push(matches);
+                        matches = [];
+                    }
+                }
             }
         }
+
         if (!found) {
             message.channel.stopTyping();
             MessageUtil.sendError(message.channel, "Sorry, I couldn't find that match :C");
         }
         message.channel.stopTyping();
         embed.setDescription(pages[page - 1]);
+        embed.setTitle(titles[title - 1]);
 
         if (pages.length > 1) {
+            embed.setFooter(`Page ${page} of ${pages.length}`);
             let mess = embed.buildEmbed().getEmbed;
             message.channel.send(mess).then(msg => {
                 msg.react("⬅").then(r => {
@@ -92,14 +155,24 @@ class MatchCommand extends Command {
                     backwards.on('collect', r => {
                         if (page === 1) return;
                         page--;
+                        if (args.length === 1) {
+                            title--;
+                            embed.setTitle(titles[title - 1]);
+                        }
                         embed.setDescription(pages[page - 1]);
+                        embed.setFooter(`Page ${page} of ${pages.length}`);
                         msg.edit(embed.buildEmbed().getEmbed);
                     })
 
                     forwards.on('collect', r => {
                         if (page === pages.length) return;
                         page++;
+                        if (args.length === 1) {
+                            title++;
+                            embed.setTitle(titles[title - 1]);
+                        }
                         embed.setDescription(pages[page - 1]);
+                        embed.setFooter(`Page ${page} of ${pages.length}`);
                         msg.edit(embed.buildEmbed().getEmbed);
                     });
                 })
@@ -109,4 +182,4 @@ class MatchCommand extends Command {
         }
     }
 }
-module.exports = MatchCommand
+module.exports = MatchCommand;
