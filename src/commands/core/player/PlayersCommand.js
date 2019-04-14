5062f3d7b8803c7a8d2311467edb53b3202d306d
acupoftee <bdiakaba@uccs.edu>
2019-04-13 19:02:27 -0600

    player with hero arg

--- src/commands/core/player/PlayersCommand.js
+++ src/commands/core/player/PlayersCommand.js
@@ -5,13 +5,15 @@ const { CompetitorManager, PlayerManager } = require('../../../models/owl_models
 const { Emojis } = require('../../../constants');
 const { MessageUtil } = require('../../../utils');
 const { MessageReaction } = require('discord.js');
+const heroes = require('../../../data/heroes.json');
+let heroColor, heroURL, heroTitle;
 
 class PlayersCommand extends Command {
     constructor() {
         super();
         this.name = 'players';
         this.description = 'Lists all Overwatch league players';
-        this.usage = 'players';
+        this.usage = 'players [hero]';
         this.aliases = [];
     }
 
@@ -20,23 +22,56 @@ class PlayersCommand extends Command {
         let pages = [];
         let page = 1, playerCount = 1;
         let players = [];
-        PlayerManager.players.sort(this.compare).forEach(player => {
-            let competitor = CompetitorManager.competitors.get(player.competitorId);
-            let teamoji = Emojis[competitor.abbreviatedName];
-            players.push(`${MessageUtil.getFlag(player.nationality)} ${teamoji} ${
-                Emojis[player.role.toUpperCase()]} ${
-                player.givenName} '**${player.name}**' ${player.familyName}`);
-            if (playerCount % 20 == 0) {
-                pages.push(players);
-                players = [];
-            }
-            playerCount++;
-        });
-        pages.push(players);
         const embed = new PachimariEmbed(client);
-        embed.setTitle('__Overwatch League Players__');
-        embed.setDescription(pages[page - 1]);
-        embed.setFooter(`Page ${page} of ${pages.length}`);
+
+        if (args.length === 1) {
+            const hero = this.getHeroName(args[0]);
+            if (hero !== undefined) {
+                for (const player of PlayerManager.players.sort(this.compare)) {
+                    let competitor = CompetitorManager.competitors.get(player.competitorId);
+                    let competitorHeroes = await PlayerManager.getHeroes(player);
+                    if (competitorHeroes.includes(hero)) {
+                        let teamoji = Emojis[competitor.abbreviatedName];
+                        players.push(`${MessageUtil.getFlag(player.nationality)} ${teamoji} ${
+                            Emojis[player.role.toUpperCase()]} ${
+                            player.givenName} '**${player.name}**' ${player.familyName}`);
+                        if (playerCount % 20 == 0) {
+                            pages.push(players);
+                            players = [];
+                        }
+                        playerCount++;
+                    }
+                }
+                embed.setTitle(`__Overwatch League Players with Time on ${heroTitle}__`);
+                embed.setDescription(pages[page - 1]);
+                embed.setFooter(`Page ${page} of ${pages.length}`);
+                embed.setColor(heroColor);
+                embed.setThumbnail(heroURL);
+            } else {
+                loading.then(message => message.delete());
+                MessageUtil.sendError(message.channel, "Sorry, I couldn't find that hero :C maybe a typo?");
+                return;
+            }
+        } else if (args.length === 0) {
+            PlayerManager.players.sort(this.compare).forEach(player => {
+                let competitor = CompetitorManager.competitors.get(player.competitorId);
+                let teamoji = Emojis[competitor.abbreviatedName];
+                players.push(`${MessageUtil.getFlag(player.nationality)} ${teamoji} ${
+                    Emojis[player.role.toUpperCase()]} ${
+                    player.givenName} '**${player.name}**' ${player.familyName}`);
+                if (playerCount % 20 == 0) {
+                    pages.push(players);
+                    players = [];
+                }
+                playerCount++;
+            });
+            pages.push(players);
+
+            embed.setTitle('__Overwatch League Players__');
+            embed.setDescription(pages[page - 1]);
+            embed.setFooter(`Page ${page} of ${pages.length}`);
+        }
+        
         let mess = embed.buildEmbed().getEmbed;
         loading.then(message => message.delete());
         message.channel.send(mess).then(msg => {
@@ -82,6 +117,23 @@ class PlayersCommand extends Command {
         if (a.name.toLowerCase() > b.name.toLowerCase())
             return 1;
         return 0;
+    }
+
+    /**
+     * Returns a hero name
+     * @param {string} val 
+     * @returns hero name
+     */
+    getHeroName(val) {
+        const key = val.toLowerCase();
+        for (let i = 0; i < heroes.length; i++) {
+            if (heroes[i].key === key) {
+                heroURL = heroes[i].portrait;
+                heroColor = heroes[i].color;
+                heroTitle = heroes[i].title;
+                return heroes[i].key;
+            } 
+        }
     }
 }
 module.exports = PlayersCommand;
