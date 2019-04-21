b9ba71fa252c7ef0ce2a1ac54251b2626c955d58
acupoftee <bdiakaba@uccs.edu>
2019-04-21 09:19:33 -0600

    updated version

--- src/bot.js
+++ src/bot.js
@@ -96,7 +96,7 @@ new Promise(function (resolve, reject) {
         ${client.users.array().length} members. Took ${((performance.now() - boot) / 1000).toFixed(0)} seconds.`);
         client.user.setPresence({
             game: {
-                name: `v1.2.5 - !help <3`
+                name: `v1.2.6 - !help <3`
             }
         })
         app.listen(process.env.PORT || 3000);
