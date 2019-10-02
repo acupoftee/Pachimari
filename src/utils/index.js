c35ca6d646c1ed3523044d96f6efdbd09a2e5d95
Tee <teedia31@gmail.com>
2019-10-02 12:00:35 -0400

    Reformat Index w/ StandardJS

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,14 @@
 'use strict'
-const Logger = require('./Logger');
-const MessageUtil = require('./MessageUtil');
-const NumberUtil = require('./NumberUtil');
-const JsonUtil = require('./JsonUtil');
-const AlertUtil = require('./AlertUtil');
+const Logger = require('./Logger')
+const MessageUtil = require('./MessageUtil')
+const NumberUtil = require('./NumberUtil')
+const JsonUtil = require('./JsonUtil')
+const AlertUtil = require('./AlertUtil')
 
 module.exports = {
-    Logger,
-    MessageUtil, 
-    NumberUtil,
-    JsonUtil,
-    AlertUtil
-};
+  Logger,
+  MessageUtil,
+  NumberUtil,
+  JsonUtil,
+  AlertUtil
+}
