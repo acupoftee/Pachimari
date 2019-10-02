522d4bab2e9834f2414da8ad85ff8d1c14328a1d
Tee <teedia31@gmail.com>
2019-10-02 16:12:56 -0400

    Enforce Standard JS in  module

--- src/db/Database.js
+++ src/db/Database.js
@@ -1,41 +1,41 @@
-'use strict';
+'use strict'
 
-require("dotenv").config();
-const { createConnection, PromiseConnection } = require("mysql2/promise");
-const { Logger } = require('../utils');
+require('dotenv').config()
+const { createConnection, PromiseConnection } = require('mysql2/promise')
+const { Logger } = require('../utils')
 
-let connection = null;
+let connection = null
 
 class Database {
-    /**
+  /**
      * Returns a database connection
      */
-    static get connection() {
-        return connection;
-    }
+  static get connection () {
+    return connection
+  }
 
-    /**
+  /**
      * @returns {PromiseConnection}
      */
-    async init() {
-        connection = await createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME
-        });
-        return this;
-    }
+  async init () {
+    connection = await createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    })
+    return this
+  }
 
-    /**
+  /**
      * @returns {PromiseConnection}
      */
-    async connect() {
-        connection.connect(function(err) {
-            if (err) throw Error(err);
-            Logger.success("Database connected");
-        });
-        return this;
-    }
+  async connect () {
+    connection.connect(function (err) {
+      if (err) throw Error(err)
+      Logger.success('Database connected')
+    })
+    return this
+  }
 }
-module.exports = Database;
+module.exports = Database
--- src/db/SettingQueries.js
+++ src/db/SettingQueries.js
@@ -1,111 +1,112 @@
-'use strict';
+'use strict'
 
-const Database = require('./Database');
-const { Logger } = require('../utils');
-const { TextChannel } = require(
-    'discord.js'
-)
+const Database = require('./Database')
+const { Logger } = require('../utils')
+// const { TextChannel } = require(
+//   'discord.js'
+// )
 
 class SettingsQueries {
-    static getOwlAnnounceChannels() {
-        return new Promise(function(resolve, reject) {
-            Database.connection.query(
-                `SELECT * FROM guilds WHERE announce_owl = 'true'`,
-                function(err, rows) {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve(rows);
-                }
-            );
-        });
-    }
-    /**
+  static getOwlAnnounceChannels () {
+    return new Promise(function (resolve, reject) {
+      Database.connection.query(
+        'SELECT * FROM guilds WHERE announce_owl = \'true\'',
+        function (err, rows) {
+          if (err) {
+            reject(err)
+          }
+          resolve(rows)
+        }
+      )
+    })
+  }
+
+  /**
      * Updates the command prefix for a specific server
-     * @param {number} id 
-     * @param {string} commandPrefix 
+     * @param {number} id
+     * @param {string} commandPrefix
      */
-    static updatePrefix(id, commandPrefix) {
-        return new Promise(function(resolve, reject) {
-            Database.connection.query(
+  static updatePrefix (id, commandPrefix) {
+    return new Promise(function (resolve, reject) {
+      Database.connection.query(
                 `UPDATE guilds
                  SET prefix='${commandPrefix}'
                  WHERE server_id=${id}`,
-                function(err, rows) {
-                    if (err) {
-                        return Logger.error(`[SQL] Could not UPDATE ${commandPrefix} into GUILD ${id}`);
-                    }
-                    Logger.success(`[SQL] UPDATE ${commandPrefix} into GUILD ${id} successful`);
+                function (err, rows) {
+                  if (err) {
+                    return Logger.error(`[SQL] Could not UPDATE ${commandPrefix} into GUILD ${id}`)
+                  }
+                  Logger.success(`[SQL] UPDATE ${commandPrefix} into GUILD ${id} successful`)
                 }
-            );
-        });
-    }
+      )
+    })
+  }
 
-    /**
-     * Updates the overwatch announcement boolean 
+  /**
+     * Updates the overwatch announcement boolean
      * for a specific server
-     * @param {number} id 
-     * @param {string} announce 
+     * @param {number} id
+     * @param {string} announce
      */
-    static updateOwlAnnouncement(id, announce) {
-        return new Promise(function(resolve, reject) {
-            Database.connection.query(
+  static updateOwlAnnouncement (id, announce) {
+    return new Promise(function (resolve, reject) {
+      Database.connection.query(
                 `UPDATE guilds
                  SET announce_owl='${announce}'
                  WHERE server_id=${id}`,
-                function(err, rows) {
-                    if (err) {
-                        return Logger.error(`[SQL] Could not UPDATE ${announce} into GUILD ${id}`);
-                    }
-                    Logger.success(`[SQL] UPDATE ${announce} into GUILD ${id} successful`);
+                function (err, rows) {
+                  if (err) {
+                    return Logger.error(`[SQL] Could not UPDATE ${announce} into GUILD ${id}`)
+                  }
+                  Logger.success(`[SQL] UPDATE ${announce} into GUILD ${id} successful`)
                 }
-            );
-        });
-    }
+      )
+    })
+  }
 
-    /**
+  /**
      * Updates the overwatch announcement channel
      * for a specific server
-     * @param {number} id 
-     * @param {TextChannel} channel 
+     * @param {number} id
+     * @param {TextChannel} channel
      */
-    static updateOwlAnnouncementChannel(id, channel) {
-        let channelId = channel.replace(/\D/g,'');
-        return new Promise(function(resolve, reject) {
-            Database.connection.query(
+  static updateOwlAnnouncementChannel (id, channel) {
+    const channelId = channel.replace(/\D/g, '')
+    return new Promise(function (resolve, reject) {
+      Database.connection.query(
                 `UPDATE guilds
                  SET announce_owl_channel='${channelId}'
                  WHERE server_id=${id}`,
-                function(err, rows) {
-                    if (err) {
-                        return Logger.error(`[SQL] Could not UPDATE ${channelId} into GUILD ${id}`);
-                    }
-                    Logger.success(`[SQL] UPDATE ${channelId} into GUILD ${id} successful`);
+                function (err, rows) {
+                  if (err) {
+                    return Logger.error(`[SQL] Could not UPDATE ${channelId} into GUILD ${id}`)
+                  }
+                  Logger.success(`[SQL] UPDATE ${channelId} into GUILD ${id} successful`)
                 }
-            );
-        });
-    }
-    
-    /**
-     * Updates the overwatch twitter boolean 
+      )
+    })
+  }
+
+  /**
+     * Updates the overwatch twitter boolean
      * for a specific server
-     * @param {number} id 
-     * @param {string} announce 
+     * @param {number} id
+     * @param {string} announce
      */
-    static updateOwlTwitter(id, announce) {
-        return new Promise(function(resolve, reject) {
-            Database.connection.query(
+  static updateOwlTwitter (id, announce) {
+    return new Promise(function (resolve, reject) {
+      Database.connection.query(
                 `UPDATE guilds
                  SET owl_twitter='${announce}'
                  WHERE server_id=${id}`,
-                function(err, rows) {
-                    if (err) {
-                        return Logger.error(`[SQL] Could not UPDATE ${announce} into GUILD ${id}`);
-                    }
-                    Logger.success(`[SQL] UPDATE ${announce} into GUILD ${id} successful`);
+                function (err, rows) {
+                  if (err) {
+                    return Logger.error(`[SQL] Could not UPDATE ${announce} into GUILD ${id}`)
+                  }
+                  Logger.success(`[SQL] UPDATE ${announce} into GUILD ${id} successful`)
                 }
-            );
-        });
-    }
+      )
+    })
+  }
 }
-module.exports = SettingsQueries;
+module.exports = SettingsQueries
