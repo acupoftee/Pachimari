d9d4133bfb61161f6e80ecbc628210f4d41f73b7
Tee <teedia31@gmail.com>
2019-10-02 10:56:48 -0400

    cleanup roster watcher

--- src/watchers/RosterWatcher.js
+++ src/watchers/RosterWatcher.js
@@ -1,30 +1,29 @@
-'use strict';
+'use strict'
 
-const { CompetitorManager, PlayerManager, Endpoints } = require('../models/owl_models');
-const { Logger, JsonUtil } = require('../utils');
+const { CompetitorManager, PlayerManager, Endpoints } = require('../models/owl_models')
+const { Logger, JsonUtil } = require('../utils')
 class RosterWatcher {
-    
-    /**
+  /**
      * Updates players heroes
      */
-     async watchForTeamSwaps() {
-        setInterval(async() => {
-            Logger.custom('ROSTER', 'Watching for roster updates')
-            for (const player of PlayerManager.players.array()) {
-                const playerBody = await JsonUtil.parse(Endpoints.get('PLAYER', player.id))
-                const latestCompetitor = playerBody.data.player.teams[0].team.id;
+  async watchForTeamSwaps () {
+    setInterval(async () => {
+      Logger.custom('ROSTER', 'Watching for roster updates')
+      for (const player of PlayerManager.players.array()) {
+        const playerBody = await JsonUtil.parse(Endpoints.get('PLAYER', player.id))
+        const latestCompetitor = playerBody.data.player.teams[0].team.id
 
-                if (latestCompetitor !== player.competitorId) {
-                    const previousCompetitor = CompetitorManager.competitors.get(player.competitorId);
-                    const competitor = CompetitorManager.competitors.get(latestCompetitor);
-                    competitor.players.set(latestCompetitor, player);
-                    competitor.players.delete(previousCompetitor);
-                    player.setCompetitorId(latestCompetitor);
-                    Logger.custom('ROSTER_UPDATE', `updated team for ${player.name} to ${CompetitorManager.competitors.get(latestCompetitor)}`)
-                }
-            }
-            Logger.custom('ROSTER', `Finished watching for roster updates`);
-        }, 300000)
-    }
+        if (latestCompetitor !== player.competitorId) {
+          const previousCompetitor = CompetitorManager.competitors.get(player.competitorId)
+          const competitor = CompetitorManager.competitors.get(latestCompetitor)
+          competitor.players.set(latestCompetitor, player)
+          competitor.players.delete(previousCompetitor)
+          player.setCompetitorId(latestCompetitor)
+          Logger.custom('ROSTER_UPDATE', `updated team for ${player.name} to ${CompetitorManager.competitors.get(latestCompetitor)}`)
+        }
+      }
+      Logger.custom('ROSTER', 'Finished watching for roster updates')
+    }, 300000)
+  }
 }
-module.exports = RosterWatcher;
+module.exports = RosterWatcher
