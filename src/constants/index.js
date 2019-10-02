65306571d864482e3f9c9d60baaf014ff54fe551
Tee <teedia31@gmail.com>
2019-10-02 16:20:00 -0400

    Enforce Standard JS in  module

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const ConsoleColors = require('./ConsoleColors');
-const LeagueLogo = require('./LeagueLogo');
-const Emojis = require('./Emojis');
-const PingGifs = require('./PingGifs');
+const ConsoleColors = require('./ConsoleColors')
+const LeagueLogo = require('./LeagueLogo')
+const Emojis = require('./Emojis')
+const PingGifs = require('./PingGifs')
 
-module.exports = { 
-    ConsoleColors,
-    LeagueLogo,
-    Emojis,
-    PingGifs
-};
+module.exports = {
+  ConsoleColors,
+  LeagueLogo,
+  Emojis,
+  PingGifs
+}
