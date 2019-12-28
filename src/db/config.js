8d21e51e2808bd838f3dd241b2497af8814f5e52
Tee <teedia31@gmail.com>
2019-12-28 10:59:21 -0500

    Add production db and dev db

--- src/db/config.js
+++ src/db/config.js
@@ -4,10 +4,10 @@
 const mongooseBaseName = 'pachimari'
 
 // create the mongodb uri for development and test
-const dev = `mongodb://localhost/${mongooseBaseName}`
+const dev = `mongodb://localhost:27017/${mongooseBaseName}`
 
 // Environment variable MONGODB_URI will be available in
-// heroku production evironment otherwise use test or development db
+// heroku production evironment otherwise use development db
 const currentDb = process.env.MONGODB_URI || dev
 
 module.exports = currentDb
