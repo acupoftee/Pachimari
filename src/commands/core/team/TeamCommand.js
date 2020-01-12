39bf2d0b24f247e3593588faaf05c35de141bde7
Tee <teedia31@gmail.com>
2020-01-11 19:44:20 -0500

    Remove schedule argument

--- src/commands/core/team/TeamCommand.js
+++ src/commands/core/team/TeamCommand.js
@@ -21,7 +21,7 @@ class TeamCommand extends Command {
     super()
     this.name = 'team'
     this.description = 'Shows information about a specific OWL team'
-    this.usage = 'team <team> [accounts|schedule]'
+    this.usage = 'team <team> [accounts]'
     this.aliases = []
   }
 
@@ -88,11 +88,8 @@ class TeamCommand extends Command {
       embed.setDescription(teamInfo)
       if (competitor.accounts.size > 0) {
         const word = competitor.accounts.size > 1 ? 'Accounts' : 'Account'
-        embed.addFields(`${competitor.accounts.size} ${word}`, `\`\`!team ${args[0]} accounts\`\``, true)
+        embed.addFields(`${competitor.accounts.size} ${word}`, `\`\`${client.prefix}team ${args[0]} accounts\`\``, true)
       }
-      embed.addFields('Schedule', `\`\`!team ${args[0]} schedule\`\``, true)
-      // loading.then(message => message.delete());
-      // embed.buildEmbed().post(message.channel);
       const mess = embed.buildEmbed().getEmbed
       loading.then(message => message.edit(mess))
     } else {
@@ -110,87 +107,8 @@ class TeamCommand extends Command {
         })
         const msg = accs.join('\n')
         embed.setDescription(msg)
-        // loading.then(message => message.delete());
-        // embed.buildEmbed().post(message.channel);
         const mess = embed.buildEmbed().getEmbed
         loading.then(message => message.edit(mess))
-      } else if (args[1].toLowerCase() === 'schedule') {
-        const matches = []
-        let stage = ''
-        const body = await JsonUtil.parse(Endpoints.get('SCHEDULE'))
-        Logger.custom('TEAM_COMMAND SCHEDULE', `Loading SCHEDULE for team ${competitor.name}`)
-
-        const currentTime = new Date().getTime()
-        let slug = null
-        for (let i = 0; i < stageData.length; i++) {
-          stage = stageData[i]
-          if (currentTime > stage.startDate && currentTime < stage.endDate) {
-            slug = stage.slug
-          }
-        }
-        // organize data by stage and week
-        body.data.stages.forEach(_stage => {
-          if (_stage.slug === slug) {
-            stage = _stage.name
-            _stage.weeks.forEach(week => {
-              week.matches.forEach(_match => {
-                if (_match.competitors[0].id === competitor.id || _match.competitors[1].id === competitor.id) {
-                  const home = CompetitorManager.competitors.get(CompetitorManager.locateTeam(_match.competitors[1].abbreviatedName))
-                  const away = CompetitorManager.competitors.get(CompetitorManager.locateTeam(_match.competitors[0].abbreviatedName))
-                  const match = new Match(_match.id, (_match.state === 'PENDING'),
-                    _match.state, _match.startDate, home, away, _match.scores[1].value, _match.scores[0].value)
-                  matches.push(match)
-                }
-              })
-            })
-          }
-        })
-
-        const daysMatch = []; const previousMatches = []
-        matches.forEach(match => {
-          const awayTitle = `${Emojis[match.away.abbreviatedName]} **${match.away.name}**`
-          const homeTitle = `**${match.home.name}** ${Emojis[match.home.abbreviatedName]}`
-          const date = momentTimezone(match.startDateTS).tz('America/Los_Angeles').format('dddd MMM Do')
-          const pacificTime = momentTimezone(match.startDateTS).tz('America/Los_Angeles').format('h:mm A z')
-          const utcTime = momentTimezone(match.startDateTS).utc().format('h:mm A z')
-          if (match.pending) {
-            daysMatch.push(`${date}\n*${pacificTime} / ${utcTime}*\n${awayTitle} vs ${homeTitle}\n`)
-          } else if (match.state === 'IN_PROGRESS') {
-            daysMatch.push(`*${pacificTime} / ${utcTime}* - ***NOW LIVE***\n[Watch full match here!](https://overwatchleague.com/en-us/)\n${awayTitle} ||${match.scoreAway} - ${match.scoreHome}|| ${homeTitle}\n`)
-          } else {
-            previousMatches.push(`${date}, ${momentTimezone(match.startDateTS).startOf('hour').fromNow()}\n*${pacificTime} / ${utcTime}*\n${awayTitle} ||${match.scoreAway} - ${match.scoreHome}|| ${homeTitle}\n`)
-          }
-        })
-        if (daysMatch.length === 0) {
-          daysMatch.push(`:x: No upcoming ${stage} matches. Check back for the next stage!`)
-        }
-        pages.push(daysMatch)
-        previousMatches.reverse()
-        pages.push(previousMatches)
-        embed.setDescription(pages[page - 1])
-        embed.setTitle(`__Upcoming ${stage} Matches for ${competitor.name}__`)
-        embed.setFooter(`Page ${page} of ${pages.length}. Only command author can turn pages`)
-        // loading.then(message => message.delete());
-        const mess = embed.buildEmbed().getEmbed
-        loading.then(message => message.edit(mess)).then(msg => {
-          msg.react('🔄').then(r => {
-            const switchFilter = (reaction, user) => reaction.emoji.name === '🔄' && user.id === message.author.id
-            const refresh = msg.createReactionCollector(switchFilter, { time: 60000 })
-            refresh.on('collect', r => {
-              if (page % 2 === 0) {
-                page--
-                embed.setTitle(`__Upcoming ${stage} Matches for ${competitor.name}__`)
-              } else {
-                page++
-                embed.setTitle(`__Previous ${stage} Matches for ${competitor.name}__`)
-              }
-              embed.setDescription(pages[page - 1])
-              embed.setFooter(`Page ${page} of ${pages.length}. Only command author can turn pages`)
-              msg.edit(embed.buildEmbed().getEmbed)
-              r.remove(message.author.id)
-            })
-          })
-        })
       } else {
         loading.then(message => message.edit(AlertUtil.ERROR(":C Sorry I couldn't understand that. Maybe a typo?")))
       }
